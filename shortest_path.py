b0b760ef6287f4977e9636d8149004965a8106ea
MourningPajamas <MourningPajamas@gmail.com>
2020-02-02 15:12:08 -0600

    Exception Handling and more docstrings

--- shortest_path.py
+++ shortest_path.py
@@ -4,39 +4,59 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def artist_prompt():
-    """ returns list of starting and ending nodes provided by user
+    """ returns list of starting and ending nodes provided by user.
+        If an artist is not found from search, the prompt is restarted.
 
     """
-
     starting_artist_prompt = input("Enter the artist you would like to start at: ")
     ending_artist_prompt = input("Enter the artist you would like to end at: ")
 
-    #Create try statements later for multiple names
-    starting_artist_search = sp.search(q=starting_artist_prompt,type='artist')
-    ending_artist_search   = sp.search(q=ending_artist_prompt,type='artist')
+    try:
 
-    starting_artist_name = starting_artist_search['artists']['items'][0]['name']
-    starting_artist_id   = starting_artist_search['artists']['items'][0]['id']
+        starting_artist_search = sp.search(q=starting_artist_prompt,type='artist')
+        ending_artist_search   = sp.search(q=ending_artist_prompt,type='artist')
 
-    ending_artist_name = ending_artist_search['artists']['items'][0]['name']
-    ending_artist_id   = ending_artist_search['artists']['items'][0]['id']
-    
+        starting_artist_name = starting_artist_search['artists']['items'][0]['name']
+        starting_artist_id   = starting_artist_search['artists']['items'][0]['id']
 
-    print("Starting at {0} and ending at {1}".format(starting_artist_name, ending_artist_name))
+        ending_artist_name = ending_artist_search['artists']['items'][0]['name']
+        ending_artist_id   = ending_artist_search['artists']['items'][0]['id']
 
-    return [starting_artist_id, ending_artist_id]
+        print("Starting at {0} and ending at {1}".format(starting_artist_name, ending_artist_name))
+        return [starting_artist_id, ending_artist_id]
+
+    except IndexError:
+        print("At least one of the artists is not on Spotify. Please try again\n")
+        return artist_prompt()
+
     
 
 def backtrack(starting_node, ending_node, solution_dictionary):
+    """ returns list of path between starting and ending node using solution_dictionary if path exists.
+        Otherwise, returns message stating that the artists are too far apart if they are not within
+        roughly the number of nodes in the graph provided in BFS function
+
+        Parameters:
+            starting_node       - id for artist to start at (string)
+
+            ending_node         - id for artist to end at (string)
+
+            solution_dictionary - Links between artists in graph which is used similar to adjacency list (dictionary) 
+
+
+    """
     id_path = [ending_node] 
 
-    # If artists are more than 1000 layers deep this will run forever (careful)
     while ending_node != starting_node:
-        id_path.append(solution_dictionary[ending_node])
-        ending_node = solution_dictionary[ending_node]
+        try:
+            id_path.append(solution_dictionary[ending_node])
+            ending_node = solution_dictionary[ending_node]
+        except KeyError:
+            return "Artists are too far apart or there is no link between them"
 
     name_path = list()
 
+    # Builds path backwards from destination to start
     for identification in id_path:
         name_path.append(sp.artist(identification)['name'])
 
@@ -52,7 +72,7 @@ def BFS(starting_node):
     frontier.append(starting_node)
     visited.add(starting_node)
 
-    while len(frontier) != 0 and len(frontier) <= 1000:
+    while len(frontier) != 0 and len(frontier) <= 5000:
         current = frontier.popleft()
 
         for artist in sp.artist_related_artists(current)['artists']:
@@ -67,6 +87,7 @@ def BFS(starting_node):
             
 
 if __name__ == "__main__":
+
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'],client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
 
     starting_and_ending = artist_prompt()
